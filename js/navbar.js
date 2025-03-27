3742e2a2ebd135a9de912d7e6481694e2b4fc99f
jembamin2 <jemb.lienard@gmail.com>
2025-03-27 15:55:34 +0100

    some changes

--- js/navbar.js
+++ js/navbar.js
@@ -94,10 +94,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index2.html">Accueil</a>
+                        <a class="nav-link active" aria-current="page" href="index.html">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="Analysis.html">Analyse Ethique</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="Enquêtes.html">Enquêtes</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link disabled" aria-disabled="true">Disabled</a>
@@ -117,7 +120,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     let footer = document.createElement("footer");
+    footer.className = "container";
     footer.innerHTML = `<p class="float-end"><a href="#">Back to top</a></p>
     <p>&copy; 2017–2024 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>`;
     document.body.appendChild(footer)
+
+    
 });
