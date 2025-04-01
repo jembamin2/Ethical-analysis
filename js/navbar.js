dd5ecb09b0ab732ea273b6b57708c6a6dde147d8
jembamin2 <jemb.lienard@gmail.com>
2025-04-01 16:34:06 +0200

    small fix on navbar

--- js/navbar.js
+++ js/navbar.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function() {
         <header data-bs-theme="dark">
             <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
                 <div class="container-fluid container px-5">
-                // <a class="navbar-brand" href="index.html">La censure des réseaux</a>
+                 <a class="navbar-brand" href="index.html">La censure des réseaux</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
